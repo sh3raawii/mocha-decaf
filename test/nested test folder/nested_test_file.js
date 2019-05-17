7dff7add0187e06c14772adbf82a228925caba73
Mostafa Mahmoud <mostafa_mahmoud@protonmail.com>
2019-05-17 23:24:11 +0200

    Add mocha as a dev dependency - Add tests - Rename cli into main - Create 2 bin entries - Add keywords for search

---
+++ test/nested test folder/nested_test_file.js
@@ -0,0 +1,5 @@
+describe('nested dummy test suite', async () => {
+  it('nested dummy test case', async () => {
+    throw new Error('Nested Test Case Body Was Executed')
+  })
+})
---
+++ test/test_file.js
@@ -0,0 +1,5 @@
+describe('dummy test suite', async () => {
+  it('dummy test case', async () => {
+    throw new Error('Test Case Body Was Executed')
+  })
+})
