ac1c95aa7d5944563c2a0ba1da83072ce8405672
Mostafa Mahmoud <mostafa_mahmoud@protonmail.com>
2019-05-17 22:55:12 +0200

    add standard for code linting

--- basic.js
+++ basic.js
@@ -4,13 +4,13 @@ const Mocha = require('mocha')
 const { listAllFiles, isJSFile, patchTestRunner } = require('./lib')
 
 const getArgs = () => {
-    if ((process.argv.length <= 2) || ((process.argv[2] == '-h') || (process.argv[2] == '--help'))){
-        console.log ("Use: mocha-dry-basic <test-dir>\n")
-        console.log ("Copyright(c) 2019 Mostafa Yassin - MIT Licensed")
-        process.exit(0);
-    }
-    const testDir = process.argv[2] || 'test';
-    return { testDir }
+  if ((process.argv.length <= 2) || ((process.argv[2] === '-h') || (process.argv[2] === '--help'))) {
+    console.log('Use: mocha-dry-basic <test-dir>\n')
+    console.log('Copyright(c) 2019 Mostafa Yassin - MIT Licensed')
+    process.exit(0)
+  }
+  const testDir = process.argv[2] || 'test'
+  return { testDir }
 }
 
 /**
@@ -20,23 +20,23 @@ const getArgs = () => {
  * Run Mocha programmatically
  */
 const main = async () => {
-const args = getArgs()
-// Instantiate a Mocha instance
-const mocha = new Mocha()
-// List all JS files and add them to mocha
-const files = listAllFiles(args.testDir)
-files.filter(isJSFile).forEach((testFile) => {
+  const args = getArgs()
+  // Instantiate a Mocha instance
+  const mocha = new Mocha()
+  // List all JS files and add them to mocha
+  const files = listAllFiles(args.testDir)
+  files.filter(isJSFile).forEach((testFile) => {
     mocha.addFile(testFile)
-})
-// Patch Mocha's test runner
-patchTestRunner()
-// Run the tests
-mocha.run((failures) => {
-  process.exitCode = failures ? 1 : 0;
-})
+  })
+  // Patch Mocha's test runner
+  patchTestRunner()
+  // Run the tests
+  mocha.run((failures) => {
+    process.exitCode = failures ? 1 : 0
+  })
 }
 
 main().catch((error) => {
-    console.error(error)
-    process.exit(-1)
+  console.error(error)
+  process.exit(-1)
 })
--- lib.js
+++ lib.js
@@ -3,39 +3,38 @@ const path = require('path')
 const fs = require('fs')
 
 /**
- * 
+ *
  * @param {String} filePath Path to a file holding filename and file extension
  */
 const isJSFile = (filePath) => filePath.substr(-3) === '.js'
 
 /**
- * 
+ *
  * @param {String} dir Path to a dir
  */
 const listAllFiles = (dir) => {
-    let JSFiles = []
-    const dirContents = fs.readdirSync(dir)
-    for (const item of dirContents) {
-        const itemPath = path.join(dir, item)
-        const itemStat = fs.statSync(itemPath)
-        if (itemStat.isFile()) JSFiles.push(itemPath)
-        else if (itemStat.isDirectory()) JSFiles = JSFiles.concat(listAllFiles(itemPath))
-    }
-    return JSFiles
+  let JSFiles = []
+  const dirContents = fs.readdirSync(dir)
+  for (const item of dirContents) {
+    const itemPath = path.join(dir, item)
+    const itemStat = fs.statSync(itemPath)
+    if (itemStat.isFile()) JSFiles.push(itemPath)
+    else if (itemStat.isDirectory()) JSFiles = JSFiles.concat(listAllFiles(itemPath))
+  }
+  return JSFiles
 }
 
 /**
  * Patch the runTest method of the Mocha Test Runner to pass all tests
  */
 const patchTestRunner = () => {
-    Mocha.Runner.prototype.runTest = (callback) => {
-        callback()
-        return
-    }
+  Mocha.Runner.prototype.runTest = (callback) => {
+    callback()
+  }
 }
 
 module.exports = {
-    isJSFile,
-    listAllFiles,
-    patchTestRunner
+  isJSFile,
+  listAllFiles,
+  patchTestRunner
 }
